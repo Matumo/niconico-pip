a9dbbf70382572be34f5d9a2d4f5adb3789d53e1
Matumo <dev@matumo.com>
2025-04-28 00:27:48 +0900

    新しい要素のIDに対応

--- chrome-extension/00-config.js
+++ chrome-extension/00-config.js
@@ -9,10 +9,15 @@ const videoPipCanvasWidth = 1920;
 const pipButtonOnMouseOverColor = '#ffffff';
 const pipButtonOnMouseOutColor = '#cccccc';
 
-const tooltipButtonElementSelector = '[id="tooltip::rb::trigger"]';
-const r3ElementSelector = '[data-styling-id=":r3:"]';
-const r5ElementSelector = '[id="menu::r5::ctx-trigger"]';
+// PIPボタンの後ろに位置する要素（コメント非表示ボタン）のセレクタ
+const tooltipButtonElementSelector = '[id="tooltip::rb::trigger"], [id="tooltip:«rb»:trigger"]';
+// PIP用のvideo要素を入れる要素のセレクタ（既存video要素と同じ要素に兄弟として入れる）
+const r3ElementSelector = '[data-styling-id=":r3:"], [data-styling-id="«r3»"]';
+// 既存video要素とコメントキャンバスが入っているコンテナ要素のセレクタ（動画が変わるとこの要素が変わる）
+const r5ElementSelector = '[id="menu::r5::ctx-trigger"], [id="menu:«r5»:ctx-trigger"]';
+// 既存video要素のセレクタ
 const nicoVideoElementSelector = '[data-name="content"] > video';
+// コメントキャンバスのセレクタ
 const nicoCommentsElementSelector = 'div[data-name="comment"] > canvas';
 
 const waitForElementsIntervalTime = 100;
