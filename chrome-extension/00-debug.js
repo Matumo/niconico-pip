ca49303aa307fff5a8bbc4744943d68541e07568
Matumo <dev@matumo.com>
2025-05-26 01:45:28 +0900

    追跡除外設定のコメントを追加

--- chrome-extension/00-debug.js
+++ chrome-extension/00-debug.js
@@ -1,2 +1,7 @@
 // デバッグモード設定
 const debugMode = false;
+
+// gitの追跡除外設定（ローカル設定）
+//   git update-index --assume-unchanged chrome-extension/00-debug.js
+// 設定解除
+//   git update-index --no-assume-unchanged chrome-extension/00-debug.js
